9e5efdb5640ad7a8d83556dd2871fcb76915bbc7
jgustavoj <gjime011@fiu.edu>
2022-04-04 17:56:24 -0400

    input fields and headings done and responsive

---
+++ src/front/js/component/InputField.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export const InputField = () => {
+  return (
+    <>
+      <form>
+        <div className="row">
+          <div className="form-group col-md-6">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First Name"
+              autoFocus
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last Name"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="form-group col-md-6">
+            <input type="text" className="form-control" placeholder="Title" />
+          </div>
+          <div className="form-group col-md-6">
+            <input type="email" className="form-control" placeholder="Email" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="form-group col-md-12">
+            <textarea
+              class="form-control"
+              rows="10"
+              placeholder="Message"></textarea>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
--- src/front/js/pages/Contact.js
+++ src/front/js/pages/Contact.js
@@ -6,64 +6,65 @@ import { About } from "../component/About.js";
 import { HeadingTwo } from "../component/HeadingTwo.js";
 import { ResponsiveImage } from "../component/ResponsiveImage.js";
 import Logo from "../../img/Logo.png";
+import { InputField } from "../component/InputField.js";
 
 export const Contact = () => {
   return (
     <>
       <div className="container-fluid p-0">
         {/* <div className="container container--pall contact__container"> */}
-        <div className="contact__container">
-          <div className="row">
-            <div className="col main-left-side">
-              <div className="row top">
-                <div className="col">
-                  <ResponsiveImage src={Logo} />
-                  -- top left --
+        <div className="contact">
+          <div className="contact__container">
+            <div className="row">
+              <div className="col contact__nav-heading-body-container">
+                <div className="row contact__nav">
+                  <div className="col">
+                    {/* -- top left  -- */}
+                    <ResponsiveImage src={Logo} />
+                  </div>
+                  <div className="col contact__link-mobile-view w700">
+                    <Link to="/">home</Link>
+                  </div>
                 </div>
-                <div className="col contact__link-mobile-view">
-                  <Link to="/">home</Link>
+                <div className="row middle">
+                  <div className="col">
+                    <section className="contact__body">
+                      {/* -- middle left -- */}
+                      <HeadingOne />
+                      <About />
+                    </section>
+                  </div>
+                </div>
+                <div className="row bottom">
+                  <div className="col">{/* -- bottom left -- */}</div>
                 </div>
               </div>
-              <div className="row middle">
-                <div className="col">
-                  <section className="contact">
-                    -- middle left --
-                    <HeadingOne />
-                    <About />
-                  </section>
-                </div>
-              </div>
-              <div className="row bottom">
-                <div className="col">-- bottom left --</div>
-              </div>
-            </div>
 
-            <div
-              className="col main-right-side"
-              style={{
-                backgroundColor: "white",
-                height: "100vh",
-                color: "black",
-              }}>
-              <div className="row">
-                <div className="col contact__link-desktop-view">
-                  <Link to="/">home</Link>
-                  -- top right --
+              <div className="col contact__heading-input-container">
+                <div className="row">
+                  <div className="col contact__link-desktop-view w700">
+                    <Link to="/">home</Link>
+                    {/* -- top right -- */}
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <section className="contact__message">
+                <div className="row">
+                  <div className="col contact__heading-two">
+                    {/* <section className="contact__message"> */}
                     <HeadingTwo style={{ color: "black" }} />
-                  </section>
-                  --middle right --
+                    {/* </section> */}
+                    {/* --middle right -- */}
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col">-- bottom right --</div>
+                <div className="row">
+                  <div className="col contact__input-container">
+                    {/* <div>input container</div> */}
+                    <InputField />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          {/* </div> */}
         </div>
       </div>
     </>
