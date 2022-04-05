8700a4e76a380ad0855da18d8519a8858e6b9b44
jgustavoj <gjime011@fiu.edu>
2022-04-04 22:36:48 -0400

    contact page almost complete, might make a dynamic reusbale component for the input fields - pending

--- src/front/js/pages/Contact.js
+++ src/front/js/pages/Contact.js
@@ -13,31 +13,29 @@ export const Contact = () => {
   return (
     <>
       <div className="container-fluid p-0">
-        {/* <div className="container container--pall contact__container"> */}
         <div className="contact">
           <div className="contact__container">
             <div className="row">
               <div className="col contact__nav-heading-body-container">
                 <div className="row contact__nav">
-                  <div className="col">
-                    {/* -- top left  -- */}
+                  <div className="col contact__logo">
                     <ResponsiveImage src={Logo} />
                   </div>
                   <div className="col contact__link-mobile-view w700">
                     <Link to="/">home</Link>
                   </div>
                 </div>
+
                 <div className="row middle">
                   <div className="col">
                     <section className="contact__body">
-                      {/* -- middle left -- */}
                       <HeadingOne />
                       <About />
                     </section>
                   </div>
                 </div>
                 <div className="row bottom">
-                  <div className="col">{/* -- bottom left -- */}</div>
+                  <div className="col"></div>
                 </div>
               </div>
 
@@ -45,35 +43,24 @@ export const Contact = () => {
                 <div className="row">
                   <div className="col contact__link-desktop-view w700">
                     <Link to="/">home</Link>
-                    {/* -- top right -- */}
                   </div>
                 </div>
                 <div className="row">
                   <div className="col contact__heading-two">
-                    {/* <section className="contact__message"> */}
                     <HeadingTwo style={{ color: "black" }} />
-                    {/* </section> */}
-                    {/* --middle right -- */}
                   </div>
                 </div>
                 <div className="row">
-                  {/* <div className="col-md-2"></div> */}
                   <div className="col-md contact__input-container">
-                    {/* <div>input container</div> */}
                     <InputField />
                     <div className="contact__button">
                       <Button value={"Submit"} />
                     </div>
                   </div>
                 </div>
-                {/* <div className="col-md-2"></div> */}
-                {/* <div className="row">
-                  <div className="col contact__button"></div>
-                </div> */}
               </div>
             </div>
           </div>
-          {/* </div> */}
         </div>
       </div>
     </>
