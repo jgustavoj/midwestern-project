90cc6e6eed1f7dcc809d811c1d2fc525b4b7facf
jgustavoj <gjime011@fiu.edu>
2022-04-07 22:49:23 -0400

    challenge complete

--- src/front/js/component/Challenge.js
+++ src/front/js/component/Challenge.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/_challenge.scss";
 import { HeadingOne } from "./HeadingOne.js";
 
 export const Challenge = (props) => {
+  const { store, actions } = useContext(Context);
+  const [unique, setUnique] = useState([]);
+  const [count, setCount] = useState(0);
+
+  function countHandler() {
+    setCount(count + 1);
+  }
+
+  function handleUnique(array1, array2) {
+    setUnique([...new Set([...array1, ...array2])]);
+  }
+
   return (
     <>
       <HeadingOne body={props.headingBody} />
       <div className="challenge__body">
         Remove the duplicates in 2 Javascript objects (found in readme), add the
         results to an array and output the list of distinct names in an
-        unordered list below this paragraph when <a href="#">this link</a> is
-        clicked. If the operation has been completed already, notify the user
+        unordered list below this paragraph when{" "}
+        <a
+          href="#"
+          onClick={() => {
+            handleUnique(store.array1, store.array2);
+            countHandler();
+          }}>
+          this link
+        </a>{" "}
+        {""}
+        is clicked. If the operation has been completed already, notify the user
         that this has already been done.
+        <br />
+        <br />
+        {unique.map((item, index) => {
+          return (
+            <ul key={index}>
+              <li>{item}</li>
+            </ul>
+          );
+        })}
+        {count > 1 ? alert("The operation has been completed already") : null}
       </div>
     </>
   );
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,24 +2,23 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       data: [],
-      demo: [
-        {
-          title: "FIRST",
-          background: "white",
-          initial: "white",
-        },
-        {
-          title: "SECOND",
-          background: "white",
-          initial: "white",
-        },
+      array1: [
+        "Matt Johnson",
+        "Matt Johnson",
+        "Bart Paden",
+        "Ryan Doss",
+        "Jared Malcolm",
+      ],
+      array2: [
+        "Matt Johnson",
+        "Bart Paden",
+        "Bart Paden",
+        "Jordan Heigle",
+        "Jordan Heigle",
+        "Tyler Viles",
       ],
     },
     actions: {
-      // Use getActions to call a function within a fuction
-      exampleFunction: () => {
-        getActions().changeColor(0, "green");
-      },
       submitForm: (contactInputData) => {
         fetch(process.env.BACKEND_URL + "/api/contact", {
           method: "POST",
@@ -44,40 +43,12 @@ const getState = ({ getStore, getActions, setStore }) => {
       },
 
       getData: () => {
-        // fetching data from the backend
-        // fetch(process.env.BACKEND_URL + "/api/home")
-        // const fetchData = async () => {
-        //   try {
-        //     const response = await fetch(process.env.BACKEND_URL + "/api/home");
-        //     const data = await response.json();
-        //     // console.log("data", data);
-        //     setStore({ data: data.content });
-        //   } catch (error) {
-        //     console.log("error", error);
-        //   }
-        // };
-        // fetchData();
-
         fetch(process.env.BACKEND_URL + "/api/home")
           .then((resp) => resp.json())
           .then((data) => setStore({ data: data.content }))
           .catch((error) =>
             console.error("Error loading message from backend", error)
           );
-      },
-      changeColor: (index, color) => {
-        //get the store
-        const store = getStore();
-
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
-
-        //reset the global store
-        setStore({ demo: demo });
       },
     },
   };
